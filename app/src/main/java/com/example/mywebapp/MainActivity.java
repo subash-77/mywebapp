5d4241fd3c7c06070805a00b6932fc8419cf7d6b
subash <ssubash56781@gmail.com>
2023-01-15 19:51:16 +0530

    finshed

---
+++ app/src/main/java/com/example/mywebapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.mywebapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    public WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.website);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://foods-service.000webhostapp.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack())
+        {
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
